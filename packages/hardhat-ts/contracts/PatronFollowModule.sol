9cac0d0ba6115740d156e82db50163147d984dea
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-03-18 23:18:24 -0400

    Initial commit of aave lens contracts.  and PatreonFollowModule

--- packages/hardhat-ts/contracts/PatronFollowModule.sol
+++ packages/hardhat-ts/contracts/PatronFollowModule.sol
@@ -3,15 +3,16 @@ pragma solidity 0.8.10;
 
 import "hardhat/console.sol";
 import { IFollowModule } from "./lens-protocol/interfaces/IFollowModule.sol";
+import { ModuleBase } from "./lens-protocol/core/modules/ModuleBase.sol";
 import { FeeModuleBase } from "./lens-protocol/core/modules/FeeModuleBase.sol";
 import { FollowValidatorFollowModuleBase } from "./lens-protocol/core/modules/follow/FollowValidatorFollowModuleBase.sol";
 
 // test profile address: 0xb212a727DD414c9cc309F7C663c5753D170AFF28
 
-contract PatronFollowModule is IFollowModule, FollowValidatorFollowModuleBase {
+contract PatronFollowModule is IFollowModule, FeeModuleBase {
   string public purpose = "Building Unstoppable Apps!!!";
 
-  constructor(address hub) FeeModuleBase(hub) {
+  constructor(address hub, address globals) ModuleBase(hub) FeeModuleBase(globals) {
     // what should we do on deploy?
   }
 
