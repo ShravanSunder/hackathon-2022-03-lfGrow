7be710dbfab471d1438231bbe114d107573b5dd7
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-03-19 22:42:28 -0400

    deploy code

--- packages/hardhat-ts/deploy/00_deploy_patronFollowModule.ts
+++ packages/hardhat-ts/deploy/00_deploy_patronFollowModule.ts
@@ -5,10 +5,10 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  await deploy('patronFollowModule', {
+  await deploy('PatronFollowModule', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
-    // args: ["Hello"],
+    args: ['0x7c86e2a63941442462cce73EcA9F07F4Ad023261'],
     log: true,
   });
 
@@ -17,11 +17,11 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const YourContract = await ethers.getContract("YourContract", deployer);
     await YourContract.setPurpose("Hello");
     
-    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate a version of a contract at a specific address!
+    //const yourContract = await ethers.getContractAt('YourContract', "0xaAC799eC2d00C013f1F11c37E654e59B0429DF6A") //<-- if you want to instantiate aÍÍ version of a contract at a specific address!
   */
 };
 export default func;
-func.tags = ['patronFollowModule'];
+func.tags = ['PatronFollowModule'];
 
 /*
 Tenderly verification
